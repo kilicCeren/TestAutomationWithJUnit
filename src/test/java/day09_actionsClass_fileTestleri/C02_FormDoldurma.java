8729a390c111eba4f9175554ef88a2450057fb19
kilicCeren <kiliccrn07@gmail.com>
2025-01-16 23:16:24 +0300

    v9

---
+++ src/test/java/day09_actionsClass_fileTestleri/C02_FormDoldurma.java
@@ -0,0 +1,96 @@
+package day09_actionsClass_fileTestleri;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.ReusableMethods;
+import utilities.TestBaseEach;
+
+public class C02_FormDoldurma  extends TestBaseEach{
+
+    @Test
+    public void test01(){
+
+        //1- https://www.testotomasyonu.com adresine gidelim
+        driver.get("https://www.testotomasyonu.com");
+
+        //2- Account linkine tiklayin
+        driver.findElement(By.xpath("(//span[.='Account'])[1]"))
+                .click();
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(1);
+
+
+        //3- Sign Up linkine basalim
+        driver.findElement(By.xpath("//*[@class='sign-up ']"))
+                .click();
+
+        //4- Ad, soyad, mail ve sifre kutularina deger yazalim
+
+        /*
+            Formdaki tum kutulari tek tek locate etmek yerine
+            firstName kutusuna click yapip, isim gonderelim
+            sonraki kutulara gecisi de TAB tusu ile yapabiliriz
+         */
+
+        WebElement firstNameKutusu = driver.findElement(By.id("firstName"));
+
+
+        ReusableMethods.bekle(1);
+
+        actions.click(firstNameKutusu)
+                .sendKeys("Cansu")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Altinsoy")
+                .sendKeys(Keys.TAB)
+                .sendKeys("cansu@hotmail.com")
+                .sendKeys(Keys.TAB)
+                .sendKeys("12345")
+                .sendKeys(Keys.TAB)
+                .sendKeys("12345").perform();
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(1);
+
+
+        //   ve Sign Up butonuna basalim
+        driver.findElement(By.id("btn-submit-form")).click();
+
+        actions.sendKeys(Keys.PAGE_UP).perform();
+        ReusableMethods.bekle(1);
+
+
+        //5- Kaydin olusturuldugunu test edin
+
+        // Kayit yapinca bizi otomatik olarak giris sayfasina yonlendiriyor
+        // kaydin olustugunu test edebilmek icin
+        // giris yapalim
+
+        WebElement emailKutusu = driver.findElement(By.id("email"));
+        WebElement passwordKutusu = driver.findElement(By.id("password"));
+        WebElement submitButonu = driver.findElement(By.id("submitlogin"));
+
+        emailKutusu.sendKeys("cansu@hotmail.com");
+        passwordKutusu.sendKeys("12345");
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(1);
+
+        submitButonu.click();
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(1);
+
+        WebElement logoutButonu = driver.findElement(By.xpath("//span[.='Logout']"));
+
+        Assertions.assertTrue(logoutButonu.isDisplayed());
+
+        logoutButonu.click();
+
+        ReusableMethods.bekle(2);
+    }
+}
